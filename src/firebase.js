b68c85c878debef18e25804eab5e3a61df06e8c6
rodrigomortero <rodrigomortero@gmail.com>
2026-01-17 14:01:31 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
-// Importa Firebase
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Tu configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDFz376Q836_lhBgf4rFRXEn9HgS3JZz2E",
   authDomain: "app-polletecar.firebaseapp.com",
@@ -13,7 +11,6 @@ const firebaseConfig = {
   appId: "1:977283664483:web:dfbd5518c3ac19d7acc656"
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
