4103de0e7c62791be31ea05b56f8626b45f497c1
rodrigomortero <rodrigomortero@gmail.com>
2026-01-18 18:01:55 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -13,6 +13,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, db };
