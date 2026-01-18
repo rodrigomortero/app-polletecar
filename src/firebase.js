6ea5dd454b420cf11e4091c93a17fa77d9c7f057
rodrigomortero <rodrigomortero@gmail.com>
2026-01-18 02:31:18 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,16 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID",
+  apiKey: "AIzaSyDFz376Q836_lhBgf4rFRXEn9HgS3JZz2E",
+  authDomain: "app-polletecar.firebaseapp.com",
+  projectId: "app-polletecar",
+  storageBucket: "app-polletecar.firebasestorage.app",
+  messagingSenderId: "977283664483",
+  appId: "1:977283664483:web:dfbd5518c3ac19d7acc656"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+
